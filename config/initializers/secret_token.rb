3d7150b6fe0c5b824c01f435db94245cdf21933f
Pierre Lasante <pierre.lasante@videotron.ca>
2011-08-14 10:26:36 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Santequebec::Application.config.secret_token = '587214c13ff051d14cd137e0e4a21aeb1a2c0d5bab86f550e90750b2ca8c9c19d1c845750924510b1c2f1194e575cdef4cb9c30b15b01eaa3582ce5f5c9fa8cc'
