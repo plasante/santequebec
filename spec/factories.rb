4d0aff4936b1372c45278ad9913900ae05a23af2
Pierre Lasante <pierre.lasante@videotron.ca>
2011-08-27 17:57:34 -0400

    Added FactoryGirl

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+    user.first_name            "Pierre"
+    user.last_name             "Lasante"
+    user.email                 "plasante@email.com"
+    user.password              "179317"
+    user.password_confirmation "179317"
+end
