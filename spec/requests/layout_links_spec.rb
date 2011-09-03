e723da2a5e05ed0aa1bff4687981bfb7bfbdf12c
Pierre Lasante <pierre.lasante@videotron.ca>
2011-09-03 13:05:04 -0400

    Reached 9.4.4

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -19,4 +19,32 @@ describe "LayoutLinks" do
     get '/signup'
     response.should have_selector('title', :content => %(Inscription))
   end
+  
+  describe "when not signed in" do
+    it "should have a signin link" do
+      visit root_path
+      response.should have_selector("a", :href => signin_path,
+                                         :content => %(Connexion))
+    end
+  end # of when not signed in
+  
+  describe "when signed in" do
+    before(:each) do
+      @user = Factory(:user)
+      visit signin_path
+      fill_in :session_email,    :with => @user.email
+      fill_in :session_password, :with => @user.password
+      click_button
+    end
+    it "should have a signout link" do
+      visit root_path
+      response.should have_selector("a", :href => signout_path,
+                                         :content => %(Quitter))
+    end
+    it "should have a profile link" do
+      visit root_path
+      response.should have_selector("a", :href => user_path(@user),
+                                         :content => %(Profil))
+    end
+  end # of when signed in
 end
