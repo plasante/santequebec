186144b6ca85cd84d086d1c889469f755f289a20
Pierre Lasante <pierre.lasante@videotron.ca>
2011-09-03 11:08:43 -0400

    Reached 9.4 Signing out

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -39,11 +39,22 @@ describe SessionsController do
     
     describe "with valid email and password" do
       before(:each) do
+        # The Factory(:user) will create a user in the test db.
         @user = Factory(:user)
         @attr = { :email => @user.email, :password => @user.password }
       end
-      it "should sign the user in"
-      it "should redirect to the user show page"
+      
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+      
     end # of valid signin
   end # of POST :create
 
